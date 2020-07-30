0c1556f12683880556280f3cf3e38a8b1f279fb5
Hooman <hesamyan@outlook.com>
2020-07-30 10:32:46 +0400

    Create window_problem.py

---
+++ window_problem.py
@@ -0,0 +1,35 @@
+# import dependencies
+from math import ceil, floor
+
+# 
+def create_tuple(current_page, number_all_pages, window_size):
+    if current_page <= window_size / 2:
+        return (1, window_size)
+    if current_page >= (number_all_pages - window_size / 2):
+        return (number_all_pages - window_size + 1, y)
+    return (current_page-window_size/2, current_page+window_size/2)
+
+
+def create_list(x, y, z):
+    (start, end) = create_tuple(x, y, z)
+
+    return [n for n in range(ceil(start), floor(end) + 1)]
+
+
+def assert_value(thing, expected):
+    if thing != expected:
+        print(f"expected: {expected} but got {thing}")
+    else:
+        print(f"Passed {thing}")
+
+
+assert_value(create_list(1, 100, 5), [1, 2, 3, 4, 5])
+assert_value(create_list(2, 100, 5), [1, 2, 3, 4, 5])
+assert_value(create_list(3, 100, 5), [1, 2, 3, 4, 5])
+assert_value(create_list(4, 100, 5), [2, 3, 4, 5, 6])
+assert_value(create_list(5, 100, 5), [3, 4, 5, 6, 7])
+
+assert_value(create_list(50, 100, 5), [48, 49, 50, 51, 52])
+
+assert_value(create_list(99, 100, 5), [96, 97, 98, 99, 100])
+assert_value(create_list(100, 100, 5), [96, 97, 98, 99, 100])
